602141fd903c06e8fddfcce5d23145b7d1a0128f
vladimirholy <vladimir.holy@post.cz>
2022-08-07 14:09:41 +0200

    Correcting the score

--- R/distr_skellam_meanvar.R
+++ R/distr_skellam_meanvar.R
@@ -77,7 +77,7 @@ distr_skellam_meanvar_score <- function(y, f) {
   s[s <= abs(m)] <- NA_real_
   bi <- (besselI(x = sqrt(s^2 - m^2), nu = y - 1) + besselI(x = sqrt(s^2 - m^2), nu = y + 1)) / besselI(x = sqrt(s^2 - m^2), nu = y)
   res_score <- matrix(0, nrow = t, ncol = 2L)
-  res_score[, 1] <- y - m#(s * y) / (s^2 - m^2) - m / (2 * sqrt(s^2 - m^2)) * bi
+  res_score[, 1] <- (s * y) / (s^2 - m^2) - m / (2 * sqrt(s^2 - m^2)) * bi
   res_score[, 2] <- -(m * y) / (s^2 - m^2) + s / (2 * sqrt(s^2 - m^2)) * bi - 1
   return(res_score)
 }
