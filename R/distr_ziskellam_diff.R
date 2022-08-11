730e16f90186d806d4724bad7d39b3186865cd90
vladimirholy <vladimir.holy@post.cz>
2022-08-11 18:26:10 +0200

    Adding the zero-inflated Skellam distribution

---
+++ R/distr_ziskellam_diff.R
@@ -0,0 +1,141 @@
+
+# ZERO-INFLATED SKELLAM DISTRIBUTION / DIFFERENCE PARAMETRIZATION
+
+
+# Parameters Function ----------------------------------------------------------
+distr_ziskellam_diff_parameters <- function(n) {
+  group_of_par_names <- c("rate", "rate", "infl")
+  par_names <- c("rate1", "rate2", "infl")
+  par_support <- c("positive", "positive", "probability")
+  res_parameters <- list(group_of_par_names = group_of_par_names, par_names = par_names, par_support = par_support)
+  return(res_parameters)
+}
+# ------------------------------------------------------------------------------
+
+
+# Density Function -------------------------------------------------------------
+distr_ziskellam_diff_density <- function(y, f) {
+  t <- nrow(f)
+  r1 <- f[, 1, drop = FALSE]
+  r2 <- f[, 2, drop = FALSE]
+  p <- f[, 3, drop = FALSE]
+  res_density <- suppressWarnings((y == 0L) * p + (1 - p) * exp(-(r1 + r2)) * (r1 / r2)^(y / 2) * besselI(x = 2 * sqrt(r1 * r2), nu = y))
+  res_density[!is.finite(res_density)] <- -Inf
+  return(res_density)
+}
+# ------------------------------------------------------------------------------
+
+
+# Log-Likelihood Function ------------------------------------------------------
+distr_ziskellam_diff_loglik <- function(y, f) {
+  t <- nrow(f)
+  r1 <- f[, 1, drop = FALSE]
+  r2 <- f[, 2, drop = FALSE]
+  p <- f[, 3, drop = FALSE]
+  res_loglik <- matrix(0, nrow = t, ncol = 1L)
+  res_loglik[y != 0L, ] <- suppressWarnings(log(1 - p[y != 0L, ]) + y[y != 0L, ] / 2 * log(r1[y != 0L, ] / r2[y != 0L, ]) - r1[y != 0L, ] - r2[y != 0L, ] + log(besselI(x = 2 * sqrt(r1[y != 0L, ] * r2[y != 0L, ]), nu = y[y != 0L, ])))
+  res_loglik[y == 0L, ] <- suppressWarnings(log(p[y == 0L, ] + (1 - p[y == 0L, ]) * exp(-(r1[y == 0L, ] + r2[y == 0L, ])) * besselI(x = 2 * sqrt(r1[y == 0L, ] * r2[y == 0L, ]), nu = 0)))
+  res_loglik[!is.finite(res_loglik)] <- -Inf
+  return(res_loglik)
+}
+# ------------------------------------------------------------------------------
+
+
+# Mean Function ----------------------------------------------------------------
+distr_ziskellam_diff_mean <- function(f) {
+  t <- nrow(f)
+  r1 <- f[, 1, drop = FALSE]
+  r2 <- f[, 2, drop = FALSE]
+  p <- f[, 3, drop = FALSE]
+  res_mean <- (1 - p) * (r1 - r2)
+  return(res_mean)
+}
+# ------------------------------------------------------------------------------
+
+
+# Variance Function ------------------------------------------------------------
+distr_ziskellam_diff_var <- function(f) {
+  t <- nrow(f)
+  r1 <- f[, 1, drop = FALSE]
+  r2 <- f[, 2, drop = FALSE]
+  p <- f[, 3, drop = FALSE]
+  res_var <- (1 - p) * (p * (r1 - r2)^2 + r1 + r2)
+  res_var <- array(res_var, dim = c(t, 1, 1))
+  return(res_var)
+}
+# ------------------------------------------------------------------------------
+
+
+# Score Function ---------------------------------------------------------------
+distr_ziskellam_diff_score <- function(y, f) {
+  t <- nrow(f)
+  r1 <- f[, 1, drop = FALSE]
+  r2 <- f[, 2, drop = FALSE]
+  p <- f[, 3, drop = FALSE]
+  bi_0 <- besselI(x = 2 * sqrt(r1 * r2), nu = 0)
+  bi_1 <- besselI(x = 2 * sqrt(r1 * r2), nu = 1)
+  res_score <- matrix(0, nrow = t, ncol = 3L)
+  res_score[, 1] <- (y == 0L) * (((p - 1) * (sqrt(r1 * r2) * bi_0 - r2 * bi_1)) / (sqrt(r1 * r2) * (p * exp(r1 + r2) + (1 - p) * bi_0))) + (y != 0L) * (sqrt(r2 / r1) * besselI(x = 2 * sqrt(r1 * r2), nu = y - 1) / besselI(x = 2 * sqrt(r1 * r2), nu = y) - 1)
+  res_score[, 2] <- (y == 0L) * (((p - 1) * (sqrt(r1 * r2) * bi_0 - r1 * bi_1)) / (sqrt(r1 * r2) * (p * exp(r1 + r2) + (1 - p) * bi_0))) + (y != 0L) * (sqrt(r1 / r2) * besselI(x = 2 * sqrt(r1 * r2), nu = y - 1) / besselI(x = 2 * sqrt(r1 * r2), nu = y) - y / r2 - 1)
+  res_score[, 3] <- (y == 0L) * ((exp(r1 + r2) - bi_0) / (p * exp(r1 + r2) + (1 - p) * bi_0)) + (y != 0L) * (1 / (p - 1))
+  return(res_score)
+}
+# ------------------------------------------------------------------------------
+
+
+# Fisher Information Function --------------------------------------------------
+distr_ziskellam_diff_fisher <- function(f) {
+  t <- nrow(f)
+  r1 <- f[, 1, drop = FALSE]
+  r2 <- f[, 2, drop = FALSE]
+  p <- f[, 3, drop = FALSE]
+  res_fisher <- array(0, dim = c(t, 3L, 3L))
+  res_fisher[, 1, 1] <- NA
+  res_fisher[, 1, 2] <- NA
+  res_fisher[, 1, 3] <- NA
+  res_fisher[, 2, 1] <- res_fisher[, 1, 2]
+  res_fisher[, 2, 2] <- NA
+  res_fisher[, 2, 3] <- NA
+  res_fisher[, 3, 1] <- res_fisher[, 1, 3]
+  res_fisher[, 3, 2] <- res_fisher[, 2, 3]
+  res_fisher[, 3, 3] <- NA
+  return(res_fisher)
+}
+# ------------------------------------------------------------------------------
+
+
+# Random Generation Function ---------------------------------------------------
+distr_ziskellam_diff_random <- function(t, f) {
+  r1 <- f[1]
+  r2 <- f[2]
+  p <- f[3]
+  res_random <- sample(c(0L, NA_real_), size = t, replace = TRUE, prob = c(p, 1 - p))
+  res_random[is.na(res_random)] <- suppressWarnings(stats::rpois(sum(is.na(res_random)), lambda = r1) - stats::rpois(sum(is.na(res_random)), lambda = r2))
+  res_random <- matrix(res_random, nrow = t, ncol = 1L)
+  return(res_random)
+}
+# ------------------------------------------------------------------------------
+
+
+# Starting Estimates Function --------------------------------------------------
+distr_ziskellam_diff_start <- function(y) {
+  y_mean <- mean(y, na.rm = TRUE)
+  y_var <- stats::var(y, na.rm = TRUE)
+  y_zero <- mean(y == 0L, na.rm = TRUE)
+  p <- 0
+  r1 <- (y_var + y_mean) / 2
+  r2 <- (y_var - y_mean) / 2
+  for (i in 1:1e3) {
+    p <- (y_zero - exp(-(r1 + r2)) * besselI(x = 2 * sqrt(r1 * r2), nu = y)) / (1 - exp(-(r1 + r2)) * besselI(x = 2 * sqrt(r1 * r2), nu = y))
+    r1 <- y_mean / (1 - p) + r2
+    r2 <- y_var / (1 - p) - p * y_mean^2 / (1 - p)^2 - r1
+  }
+  p <- max(min(p, 1 - 1e-6), 1e-6)
+  r1 <- max(r1, 1e-6)
+  r2 <- max(r2, 1e-6)
+  res_start <- c(r1, r2, p)
+  return(res_start)
+}
+# ------------------------------------------------------------------------------
+
+
--- R/distr_ziskellam_meanvar.R
+++ R/distr_ziskellam_meanvar.R
@@ -1,11 +1,11 @@
 
-# ZERO-INFLATED SKELLAM DISTRIBUTION / MEAN-DISPERSION PARAMETRIZATION
+# ZERO-INFLATED SKELLAM DISTRIBUTION / MEAN-VARIANCE PARAMETRIZATION
 
 
 # Parameters Function ----------------------------------------------------------
-distr_ziskellam_meandisp_parameters <- function(n) {
-  group_of_par_names <- c("mean", "disp", "infl")
-  par_names <- c("mean", "disp", "infl")
+distr_ziskellam_meanvar_parameters <- function(n) {
+  group_of_par_names <- c("mean", "var", "infl")
+  par_names <- c("mean", "var", "infl")
   par_support <- c("real", "positive", "probability")
   res_parameters <- list(group_of_par_names = group_of_par_names, par_names = par_names, par_support = par_support)
   return(res_parameters)
@@ -14,12 +14,15 @@ distr_ziskellam_meandisp_parameters <- function(n) {
 
 
 # Density Function -------------------------------------------------------------
-distr_ziskellam_meandisp_density <- function(y, f) {
+distr_ziskellam_meanvar_density <- function(y, f) {
   t <- nrow(f)
   m <- f[, 1, drop = FALSE]
   s <- f[, 2, drop = FALSE]
   p <- f[, 3, drop = FALSE]
-  res_density <- suppressWarnings((y == 0L) * p + (1 - p) * exp(-abs(m) - s) * ((abs(m) + m + s) / (abs(m) - m + s))^(y / 2) * besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = y))
+  m[s <= abs(m)] <- NA_real_
+  s[s <= abs(m)] <- NA_real_
+  p[s <= abs(m)] <- NA_real_
+  res_density <- suppressWarnings((y == 0L) * p + (1 - p) * exp(-s) * ((s + m) / (s - m))^(y / 2) * besselI(x = sqrt(s^2 - m^2), nu = y))
   res_density[!is.finite(res_density)] <- -Inf
   return(res_density)
 }
@@ -27,14 +30,17 @@ distr_ziskellam_meandisp_density <- function(y, f) {
 
 
 # Log-Likelihood Function ------------------------------------------------------
-distr_ziskellam_meandisp_loglik <- function(y, f) {
+distr_ziskellam_meanvar_loglik <- function(y, f) {
   t <- nrow(f)
   m <- f[, 1, drop = FALSE]
   s <- f[, 2, drop = FALSE]
   p <- f[, 3, drop = FALSE]
+  m[s <= abs(m)] <- NA_real_
+  s[s <= abs(m)] <- NA_real_
+  p[s <= abs(m)] <- NA_real_
   res_loglik <- matrix(0, nrow = t, ncol = 1L)
-  res_loglik[y != 0L, ] <- suppressWarnings(log(1 - p[y != 0L, ]) + y[y != 0L, ] / 2 * log((abs(m[y != 0L, ]) + m[y != 0L, ] + s[y != 0L, ]) / (abs(m[y != 0L, ]) - m[y != 0L, ] + s[y != 0L, ])) - abs(m[y != 0L, ]) - s[y != 0L, ] + log(besselI(x = sqrt(s[y != 0L, ]^2 + 2 * abs(m[y != 0L, ]) * s[y != 0L, ]), nu = y[y != 0L, ])))
-  res_loglik[y == 0L, ] <- suppressWarnings(log(p[y == 0L, ] + (1 - p[y == 0L, ]) * exp(-abs(m[y == 0L, ]) - s[y == 0L, ]) * besselI(x = sqrt(s[y == 0L, ]^2 + 2 * abs(m[y == 0L, ]) * s[y == 0L, ]), nu = 0)))
+  res_loglik[y != 0L, ] <- suppressWarnings(log(1 - p[y != 0L, ]) + y[y != 0L, ] / 2 * log((s[y != 0L, ] + m[y != 0L, ]) / (s[y != 0L, ] - m[y != 0L, ])) - s[y != 0L, ] + log(besselI(x = sqrt(s[y != 0L, ]^2 - m[y != 0L, ]^2), nu = y[y != 0L, ])))
+  res_loglik[y == 0L, ] <- suppressWarnings(log(p[y == 0L, ] + (1 - p[y == 0L, ]) * exp(-s[y == 0L, ]) * besselI(x = sqrt(s[y == 0L, ]^2 - m[y == 0L, ]^2), nu = 0)))
   res_loglik[!is.finite(res_loglik)] <- -Inf
   return(res_loglik)
 }
@@ -42,11 +48,14 @@ distr_ziskellam_meandisp_loglik <- function(y, f) {
 
 
 # Mean Function ----------------------------------------------------------------
-distr_ziskellam_meandisp_mean <- function(f) {
+distr_ziskellam_meanvar_mean <- function(f) {
   t <- nrow(f)
   m <- f[, 1, drop = FALSE]
   s <- f[, 2, drop = FALSE]
   p <- f[, 3, drop = FALSE]
+  m[s <= abs(m)] <- NA_real_
+  s[s <= abs(m)] <- NA_real_
+  p[s <= abs(m)] <- NA_real_
   res_mean <- (1 - p) * m
   return(res_mean)
 }
@@ -54,12 +63,15 @@ distr_ziskellam_meandisp_mean <- function(f) {
 
 
 # Variance Function ------------------------------------------------------------
-distr_ziskellam_meandisp_var <- function(f) {
+distr_ziskellam_meanvar_var <- function(f) {
   t <- nrow(f)
   m <- f[, 1, drop = FALSE]
   s <- f[, 2, drop = FALSE]
   p <- f[, 3, drop = FALSE]
-  res_var <- (1 - p) * (abs(m) + s + m^2 * p)
+  m[s <= abs(m)] <- NA_real_
+  s[s <= abs(m)] <- NA_real_
+  p[s <= abs(m)] <- NA_real_
+  res_var <- (1 - p) * (s + p * m^2)
   res_var <- array(res_var, dim = c(t, 1, 1))
   return(res_var)
 }
@@ -67,30 +79,36 @@ distr_ziskellam_meandisp_var <- function(f) {
 
 
 # Score Function ---------------------------------------------------------------
-distr_ziskellam_meandisp_score <- function(y, f) {
+distr_ziskellam_meanvar_score <- function(y, f) {
   t <- nrow(f)
   m <- f[, 1, drop = FALSE]
   s <- f[, 2, drop = FALSE]
   p <- f[, 3, drop = FALSE]
-  bi_0 <- besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = 0)
-  bi_1 <- besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = 1)
-  tri_bi_y <- (besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = y - 1) + besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = y + 1)) / besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = y)
+  m[s <= abs(m)] <- NA_real_
+  s[s <= abs(m)] <- NA_real_
+  p[s <= abs(m)] <- NA_real_
+  bi_0 <- besselI(x = sqrt(s^2 - m^2), nu = 0)
+  bi_1 <- besselI(x = sqrt(s^2 - m^2), nu = 1)
+  tri_bi_y <- (besselI(x = sqrt(s^2 - m^2), nu = y - 1) + besselI(x = sqrt(s^2 - m^2), nu = y + 1)) / besselI(x = sqrt(s^2 - m^2), nu = y)
   res_score <- matrix(0, nrow = t, ncol = 3L)
-  res_score[, 1] <- y - m#(y == 0L) * ((sign(m) * (1 - p) * (sqrt(s^2 + 2 * abs(m) * s) * bi_0 - s * bi_1)) / (sqrt(s^2 + 2 * abs(m) * s) * (p * bi_0 - p * exp(abs(m) + s) - bi_0))) + (y != 0L) * (y / (2 * abs(m) + s) + (sign(m) * s) / (2 * sqrt(s^2 + 2 * abs(m) * s)) * tri_bi_y - sign(m))
-  res_score[, 2] <- (y == 0L) * (((p - 1) * (sqrt(s^2 + 2 * abs(m) * s) * bi_0 - (abs(m) + s) * bi_1)) / (sqrt(s^2 + 2 * abs(m) * s) * (p * exp(abs(m) + s) + (1 - p) * bi_0))) + (y != 0L) * ((-m * y) / (s^2 + 2 * abs(m) * s) + (abs(m) + s) / (2 * sqrt(s^2 + 2 * abs(m) * s)) * tri_bi_y - 1)
-  res_score[, 3] <- (y == 0L) * ((exp(abs(m) + s) - bi_0) / (p * exp(abs(m) + s) + (1 - p) * bi_0)) + (y != 0L) * (1 / (p - 1))
+  res_score[, 1] <- y - m#(y == 0L) * ((m * (p - 1) * bi_1) / (sqrt(s^2 - m^2) * (p * exp(s) + (1 - p) * bi_0))) + (y != 0L) * ((s * y) / (s^2 - m^2) - m / (2 * sqrt(s^2 - m^2)) * tri_bi_y)
+  res_score[, 2] <- (y == 0L) * (((p - 1) * (sqrt(s^2 - m^2) * bi_0 - s * bi_1)) / (sqrt(s^2 - m^2) * (p * exp(s) + (1 - p) * bi_0))) + (y != 0L) * (-(m * y) / (s^2 - m^2) + s / (2 * sqrt(s^2 - m^2)) * tri_bi_y - 1)
+  res_score[, 3] <- (y == 0L) * ((exp(s) - bi_0) / (p * exp(s) + (1 - p) * bi_0)) + (y != 0L) * (1 / (p - 1))
   return(res_score)
 }
 # ------------------------------------------------------------------------------
 
 
 # Fisher Information Function --------------------------------------------------
-distr_ziskellam_meandisp_fisher <- function(f) {
+distr_ziskellam_meanvar_fisher <- function(f) {
   t <- nrow(f)
   m <- f[, 1, drop = FALSE]
   s <- f[, 2, drop = FALSE]
   p <- f[, 3, drop = FALSE]
-  tri_bi_m <- (besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = m - 1) + besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = m + 1)) / besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = m)
+  m[s <= abs(m)] <- NA_real_
+  s[s <= abs(m)] <- NA_real_
+  p[s <= abs(m)] <- NA_real_
+  tri_bi_m <- (besselI(x = sqrt(s^2 - m^2), nu = m - 1) + besselI(x = sqrt(s^2 - m^2), nu = m + 1)) / besselI(x = sqrt(s^2 - m^2), nu = m)
   res_fisher <- array(0, dim = c(t, 3L, 3L))
   res_fisher[, 1, 1] <- NA
   res_fisher[, 1, 2] <- NA
@@ -107,12 +125,12 @@ distr_ziskellam_meandisp_fisher <- function(f) {
 
 
 # Random Generation Function ---------------------------------------------------
-distr_ziskellam_meandisp_random <- function(t, f) {
+distr_ziskellam_meanvar_random <- function(t, f) {
   m <- f[1]
   s <- f[2]
   p <- f[3]
   res_random <- sample(c(0L, NA_real_), size = t, replace = TRUE, prob = c(p, 1 - p))
-  res_random[is.na(res_random)] <- suppressWarnings(stats::rpois(sum(is.na(res_random)), lambda = (abs(m) + m + s) / 2) - stats::rpois(sum(is.na(res_random)), lambda = (abs(m) - m + s) / 2))
+  res_random[is.na(res_random)] <- suppressWarnings(stats::rpois(sum(is.na(res_random)), lambda = (s + m) / 2) - stats::rpois(sum(is.na(res_random)), lambda = (s - m) / 2))
   res_random <- matrix(res_random, nrow = t, ncol = 1L)
   return(res_random)
 }
@@ -120,20 +138,20 @@ distr_ziskellam_meandisp_random <- function(t, f) {
 
 
 # Starting Estimates Function --------------------------------------------------
-distr_ziskellam_meandisp_start <- function(y) {
+distr_ziskellam_meanvar_start <- function(y) {
   y_mean <- mean(y, na.rm = TRUE)
   y_var <- stats::var(y, na.rm = TRUE)
   y_zero <- mean(y == 0L, na.rm = TRUE)
   p <- 0
   m <- y_mean
-  s <- y_var - abs(y_mean)
+  s <- y_var
   for (i in 1:1e3) {
-    p <- (y_zero - exp(-abs(m) - s) * besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = 0)) / (1 - exp(-abs(m) - s) * besselI(x = sqrt(s^2 + 2 * abs(m) * s), nu = 0))
+    p <- (y_zero - exp(-s) * besselI(x = sqrt(s^2 - m^2), nu = 0)) / (1 - exp(-s) * besselI(x = sqrt(s^2 - m^2), nu = 0))
     m <- y_mean / (1 - p)
-    s <- y_var / (1 - p) - abs(m) - m^2 * p
+    s <- y_var / (1 - p) - m^2 * p
   }
   p <- max(min(p, 1 - 1e-6), 1e-6)
-  s <- max(s, 1e-6)
+  s <- max(y_var, abs(y_mean) + 1e-6)
   res_start <- c(m, s, p)
   return(res_start)
 }
