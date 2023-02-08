f07f9b4a98fd542a398a6c7e1a2f404dffd8279f
vladimirholy <vladimir.holy@post.cz>
2023-02-08 01:16:36 +0100

    Adding the Laplace distribution

---
+++ R/distr_laplace_meanscale.R
@@ -0,0 +1,110 @@
+
+# NORMAL DISTRIBUTION / MEAN-VARIANCE PARAMETRIZATION
+
+
+# Parameters Function ----------------------------------------------------------
+distr_laplace_meanscale_parameters <- function(n) {
+  group_of_par_names <- c("mean", "scale")
+  par_names <- c("mean", "scale")
+  par_support <- c("real", "positive")
+  res_parameters <- list(group_of_par_names = group_of_par_names, par_names = par_names, par_support = par_support)
+  return(res_parameters)
+}
+# ------------------------------------------------------------------------------
+
+
+# Density Function -------------------------------------------------------------
+distr_laplace_meanscale_density <- function(y, f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_density <- exp(-abs(y - m) / s) / (2 * s)
+  return(res_density)
+}
+# ------------------------------------------------------------------------------
+
+
+# Log-Likelihood Function ------------------------------------------------------
+distr_laplace_meanscale_loglik <- function(y, f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_loglik <- -log(2 * s) - abs(y - m) / s
+  return(res_loglik)
+}
+# ------------------------------------------------------------------------------
+
+
+# Mean Function ----------------------------------------------------------------
+distr_laplace_meanscale_mean <- function(f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_mean <- m
+  return(res_mean)
+}
+# ------------------------------------------------------------------------------
+
+
+# Variance Function ------------------------------------------------------------
+distr_laplace_meanscale_var <- function(f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_var <- 2 * s^2
+  res_var <- array(res_var, dim = c(t, 1, 1))
+  return(res_var)
+}
+# ------------------------------------------------------------------------------
+
+
+# Score Function ---------------------------------------------------------------
+distr_laplace_meanscale_score <- function(y, f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_score <- matrix(0, nrow = t, ncol = 2L)
+  res_score[, 1] <- sign(y - m) / s
+  res_score[, 2] <- abs(y - m) / s^2 - 1 / s
+  return(res_score)
+}
+# ------------------------------------------------------------------------------
+
+
+# Fisher Information Function --------------------------------------------------
+distr_laplace_meanscale_fisher <- function(f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_fisher <- array(0, dim = c(t, 2L, 2L))
+  res_fisher[, 1, 1] <- 1 / s^2
+  res_fisher[, 2, 2] <- 1 / s^2
+  return(res_fisher)
+}
+# ------------------------------------------------------------------------------
+
+
+# Random Generation Function ---------------------------------------------------
+distr_laplace_meanscale_random <- function(t, f) {
+  m <- f[1]
+  s <- f[2]
+  u <- runif(n = t, min = -1, max = 1)
+  res_random <- m - s * sign(u) * log(1 - abs(u))
+  res_random <- matrix(res_random, nrow = t, ncol = 1L)
+  return(res_random)
+}
+# ------------------------------------------------------------------------------
+
+
+# Starting Estimates Function --------------------------------------------------
+distr_laplace_meanscale_start <- function(y) {
+  y_mean <- mean(y, na.rm = TRUE)
+  y_var <- stats::var(y, na.rm = TRUE)
+  m <- y_mean
+  s <- max(sqrt(y_var / 2), 1e-6)
+  res_start <- c(m, s)
+  return(res_start)
+}
+# ------------------------------------------------------------------------------
+
+
