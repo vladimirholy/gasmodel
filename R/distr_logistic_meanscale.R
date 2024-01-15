799f4ffbabe35c78a4421580f098a63a43a5c551
vladimirholy <vladimir.holy@post.cz>
2024-01-15 15:54:07 +0100

    Adding the logistic distribution

---
+++ R/distr_logistic_meanscale.R
@@ -0,0 +1,109 @@
+
+# NORMAL DISTRIBUTION / MEAN-VARIANCE PARAMETRIZATION
+
+
+# Parameters Function ----------------------------------------------------------
+distr_logistic_meanscale_parameters <- function(n) {
+  group_of_par_names <- c("mean", "scale")
+  par_names <- c("mean", "scale")
+  par_support <- c("real", "positive")
+  res_parameters <- list(group_of_par_names = group_of_par_names, par_names = par_names, par_support = par_support)
+  return(res_parameters)
+}
+# ------------------------------------------------------------------------------
+
+
+# Density Function -------------------------------------------------------------
+distr_logistic_meanscale_density <- function(y, f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_density <- be_silent(stats::dlogis(y, location = m, scale = s))
+  return(res_density)
+}
+# ------------------------------------------------------------------------------
+
+
+# Log-Likelihood Function ------------------------------------------------------
+distr_logistic_meanscale_loglik <- function(y, f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_loglik <- be_silent(stats::dlogis(y, location = m, scale = s, log = TRUE))
+  return(res_loglik)
+}
+# ------------------------------------------------------------------------------
+
+
+# Mean Function ----------------------------------------------------------------
+distr_logistic_meanscale_mean <- function(f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_mean <- m
+  return(res_mean)
+}
+# ------------------------------------------------------------------------------
+
+
+# Variance Function ------------------------------------------------------------
+distr_logistic_meanscale_var <- function(f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_var <- s^2 * pi^2 / 3
+  res_var <- array(res_var, dim = c(t, 1, 1))
+  return(res_var)
+}
+# ------------------------------------------------------------------------------
+
+
+# Score Function ---------------------------------------------------------------
+distr_logistic_meanscale_score <- function(y, f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_score <- matrix(0, nrow = t, ncol = 2L)
+  res_score[, 1] <- tanh((y - m) / (2 * s)) / s
+  res_score[, 2] <- (y - m) / s^2 * tanh((y - m) / (2 * s)) - 1 / s
+  return(res_score)
+}
+# ------------------------------------------------------------------------------
+
+
+# Fisher Information Function --------------------------------------------------
+distr_logistic_meanscale_fisher <- function(f) {
+  t <- nrow(f)
+  m <- f[, 1, drop = FALSE]
+  s <- f[, 2, drop = FALSE]
+  res_fisher <- array(0, dim = c(t, 2L, 2L))
+  res_fisher[, 1, 1] <- 1 / (3 * s^2)
+  res_fisher[, 2, 2] <- 1 / (3 * s^2) * (pi^2 / 3 + 1)
+  return(res_fisher)
+}
+# ------------------------------------------------------------------------------
+
+
+# Random Generation Function ---------------------------------------------------
+distr_logistic_meanscale_random <- function(t, f) {
+  m <- f[1]
+  s <- f[2]
+  res_random <- be_silent(stats::rlogis(t, location = m, scale = s))
+  res_random <- matrix(res_random, nrow = t, ncol = 1L)
+  return(res_random)
+}
+# ------------------------------------------------------------------------------
+
+
+# Starting Estimates Function --------------------------------------------------
+distr_logistic_meanscale_start <- function(y) {
+  y_mean <- mean(y, na.rm = TRUE)
+  y_var <- stats::var(y, na.rm = TRUE)
+  m <- y_mean
+  s <- max(sqrt(y_var * 3) / pi, 1e-6)
+  res_start <- c(m, s)
+  return(res_start)
+}
+# ------------------------------------------------------------------------------
+
+
